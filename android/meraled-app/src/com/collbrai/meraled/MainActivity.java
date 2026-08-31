bc1a1ec70e1e62bd81f38e5d71ac7baa9a62b633
Faruk Ozyurt <fzyurt13@gmail.com>
2026-08-31 11:04:51 +0300

    feat: add initial Meraled Android WebView APK client

---
+++ android/meraled-app/src/com/collbrai/meraled/MainActivity.java
@@ -0,0 +1,47 @@
+package com.collbrai.meraled;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    private static final String MERALED_URL =
+        "http://192.168.1.27:8080/splash";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
+        setContentView(webView);
+
+        webView.loadUrl(MERALED_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
